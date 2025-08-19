772f469332c2b98dc1e9e3fea6d5b8f9e6c5bc2d
marksman456 <hank6831511@yahoo.com.tw>
2025-08-19 13:34:08 +0800

    建立需要的model dto 取得需要的json

---
+++ Models/DTOs/ProductSearchDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Project.Models.DTOs
+{
+    public class ProductSearchDTO
+    {
+        public int Id { get; set; }
+        public string Label { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
