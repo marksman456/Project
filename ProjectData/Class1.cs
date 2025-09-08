e4e477a10b9e109307a44ed5a7f2e9d129e33ce3
marksman456 <hank6831511@yahoo.com.tw>
2025-09-08 10:23:23 +0800

    更新資料結構

---
+++ ProjectData/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjectData
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ProjectData/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectData.Models;
+
+public partial class Product
+{
+    public int ProductID { get; set; }
+
+    public string ProductSKU { get; set; } = null!;
+
+    public string ProductName { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public int ProductModelID { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string? ProductImage { get; set; }
+
+    public bool IsSerialized { get; set; }
+
+    public virtual ICollection<ProductDetail> ProductDetail { get; set; } = new List<ProductDetail>();
+
+    public virtual ProductModel ProductModel { get; set; } = null!;
+}
