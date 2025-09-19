ab66faa44a1aae5483b74a59455e4e5a3d631a6d
marksman456 <hank6831511@yahoo.com.tw>
2025-09-19 10:37:38 +0800

    Apply .gitignore and remove tracked build artifacts

--- ProjectData/obj/Debug/net8.0/ProjectData.AssemblyInfo.cs
+++ ProjectData/obj/Debug/net8.0/ProjectData.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2ea54d8717317095b9eb35236ab25bc9a927e35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee173efc0fe45103817ed0e06f2ba96def07dcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
